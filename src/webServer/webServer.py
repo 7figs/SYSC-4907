c2ad80d3bf7ab4db59e64fc9921315986ca0c601
gihankuru <gihan_a_k@MSI.>
2025-11-13 15:23:24 -0500

    Added cold Dislike and Cold start pages, with added images for the movies

--- src/webServer/webServer.py
+++ src/webServer/webServer.py
@@ -14,5 +14,13 @@ def coldStart():
 def chooseAccount():
     return render_template("chooseAccount.html")
 
+@app.route("/load_store_delete")
+def loadStoreDelete():
+    return render_template("load_store.html")
+
+@app.route("/cold_dislike")
+def coldDislike():
+    return render_template("coldDislike.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
